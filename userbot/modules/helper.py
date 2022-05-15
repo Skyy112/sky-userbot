d9b6d8ce58e19dd2a069342a88bdea5087241df6
Sky <93299453+Skyy112@users.noreply.github.com>
2022-05-15 19:45:04 +0700

    Update helper.py

--- userbot/modules/helper.py
+++ userbot/modules/helper.py
@@ -7,22 +7,21 @@ from userbot.utils import edit_or_reply, kyy_cmd
 async def usit(e):
     await edit_or_reply(e,
                         f"**Halo {owner} Jika Anda Tidak Tau Perintah Untuk Memerintah Ku Ketik** `{cmd}help` Atau Bisa Minta Bantuan Ke:\n"
-                        "\n[Telegram](t.me/ydhiyaa123)"
-                        "\n[Repo](https://github.com/S/sky-userbot)"
-                        "\n[Instagram](instagram.com/rizkyhamdanii16_)")
+                        "\n[Telegram](t.me/yaudahhlahhhh)"
+                        "\n[Repo](https://github.com/Skyy112/sky-userbot)"
 
 
 @kyy_cmd(pattern="vars$")
 async def var(m):
     await edit_or_reply(m,
                         f"**Disini Daftar Vars Dari {owner}:**\n"
-                        "\n[DAFTAR VARS](https://raw.githubusercontent.com/muhammadrizky16/Kyy-Userbot/Kyy-Userbot/varshelper.txt)")
+                        "\n[DAFTAR VARS](https://raw.githubusercontent.com/Skyy112/sky-userbot/main/varshelper.txt)")
 
 
 CMD_HELP.update({
     "helper":
     f"`{cmd}lhelp`\
-\nUsage: Bantuan Untuk Kyy-Userbot.\
+\nUsage: Bantuan Untuk sky-userbot.\
 \n`{cmd}vars`\
 \nUsage: Melihat Daftar Vars."
 })
